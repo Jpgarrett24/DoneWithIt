62143743d85782215cff9a2f3b4ef61cc4da2cbe
jpgarrett <jpgarrett24@gmail.com>
2021-01-15 16:52:10 -0800

    created picker component with funcitonality

--- App.js
+++ App.js
@@ -3,10 +3,27 @@ import { StyleSheet } from 'react-native';
 import colors from './app/config/colors';
 
 import Screen from "./app/components/Screen"
+import AppPicker from './app/components/AppPicker';
+import AppTextInput from "./app/components/AppTextInput";
+
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+];
 
 const App = () => {
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories} icon="apps" placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 };
