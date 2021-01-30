f1b5a074db79e19157fa690b2a99cbf5dfa65ad0
jpgarrett <jpgarrett24@gmail.com>
2021-01-29 16:26:29 -0800

    installed tab navigator module

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { StyleSheet, Button, Image } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import React, { } from 'react';
+import { Button, Text } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
 
 
 import Screen from "./app/components/Screen";
-import ListingEditScreen from './app/screens/ListingEditScreen';
+
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button title="View Tweet" onPress={() => navigation.navigate("TweetDetails")} />
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 const App = () => {
-  const [imageUris, setImageUris] = useState([]);
-
-
   return (
-    <ListingEditScreen />
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({});
 
 export default App;
 
