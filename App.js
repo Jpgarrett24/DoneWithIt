981455c359a50a43c4aa8a3e7cda8f5c2b8b1058
jpgarrett <jpgarrett24@gmail.com>
2021-02-01 09:52:19 -0800

    Created Authentication Navigator for welcome/login/register screens. Also added custom themes to NavigationContainer

--- App.js
+++ App.js
@@ -1,36 +1,13 @@
 import React, { } from 'react';
-import { Button, Text } from 'react-native';
-import { createStackNavigator } from '@react-navigation/stack'
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 
-
-import Screen from "./app/components/Screen";
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Button title="View Tweet" onPress={() => navigation.navigate("TweetDetails")} />
-  </Screen>
-);
-
-const TweetDetails = () => (
-  <Screen>
-    <Text>Tweet Details</Text>
-  </Screen>
-)
-
-const Stack = createStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </Stack.Navigator>
-);
+import AuthNavigator from './app/navigation/AuthNavigator';
+import navigationTheme from './app/navigation/navigationTheme';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
+    <NavigationContainer theme={navigationTheme}>
+      <AuthNavigator />
     </NavigationContainer>
   );
 };
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -3,7 +3,7 @@ import { ImageBackground, Image, View, Text, StyleSheet, Platform } from 'react-
 
 import AppButton from '../components/AppButton';
 
-const WelcomeScreen = (props) => {
+const WelcomeScreen = ({ navigation }) => {
     return (
         <ImageBackground
             source={require("../assets/background.jpg")}
@@ -15,8 +15,8 @@ const WelcomeScreen = (props) => {
                 <Text style={styles.text}>Sell What You Don't Need</Text>
             </View>
             <View style={styles.buttonsContainer}>
-                <AppButton title="LOGIN" />
-                <AppButton title="REGISTER" color="secondary" />
+                <AppButton title="Login" onPress={() => navigation.navigate("Login")} />
+                <AppButton title="Register" color="secondary" onPress={() => navigation.navigate("Register")} />
             </View>
         </ImageBackground>
     );
