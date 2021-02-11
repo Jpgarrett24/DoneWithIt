7e6d267f31e9881178bbb008a2c1cbc84cf7f591
jpgarrett <jpgarrett24@gmail.com>
2021-02-11 13:39:13 -0800

    Added register user functionality with new api/users function.

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
 import * as Yup from 'yup';
 
 import Screen from "../components/Screen";
-import { AppForm, AppFormField, SubmitButton } from "../components/forms/index";
+import { AppForm, AppFormField, ErrorMessage, SubmitButton } from "../components/forms/index";
+import useAuth from '../auth/useAuth';
+import authApi from '../api/auth';
+import usersApi from '../api/users';
 
 const validationSchema = Yup.object().shape({
     name: Yup.string().required().label("Name"),
@@ -12,13 +15,36 @@ const validationSchema = Yup.object().shape({
 })
 
 const RegisterScreen = () => {
+    const auth = useAuth();
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (userInfo) => {
+        const result = await usersApi.register(userInfo);
+
+        if (!result.ok) {
+            if (result.data) setError(result.data.error);
+            else {
+                setError("An unexpected error occured.");
+                console.log(result);
+            }
+            return;
+        }
+
+        const { data: authToken } = await authApi.login(
+            userInfo.email,
+            userInfo.password
+        );
+        auth.logIn(authToken);
+    }
+
     return (
         <Screen style={styles.container}>
             <AppForm
                 initialValues={{ name: "", email: "", password: "" }}
-                onSubmit={values => console.log(values)}
+                onSubmit={(values) => handleSubmit(values)}
                 validationSchema={validationSchema}
             >
+                <ErrorMessage error={error} visible={error} />
                 <AppFormField
                     autoCapitalize="none"
                     autoCorrect={false}
