0b609657d466b87b751b4f596692a2514e467a18
jpgarrett <jpgarrett24@gmail.com>
2021-02-11 19:05:08 -0800

    Allow users to send messages to sellers on the ListingDetailsScreen. Local notification appears when message is succesffuly sent.

---
+++ app/components/ContactSellerForm.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Keyboard, Alert } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Yup from "yup";
+
+import messagesApi from "../api/messages";
+import { AppForm, AppFormField, SubmitButton } from './forms'
+
+const validationSchema = Yup.object().shape({
+    message: Yup.string().required().min(1).label("Message",)
+});
+
+const ContactSellerForm = ({ listing }) => {
+    Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+            shouldShowAlert: true,
+            shouldPlaySound: true,
+            shouldSetBadge: false,
+        }),
+    });
+
+    const handleSubmit = async ({ message }, { resetForm }) => {
+        Keyboard.dismiss();
+
+        const result = await messagesApi.send(message, listing.id);
+
+        if (!result.ok) {
+            console.log("Error", result);
+            return Alert.alert("Error", "Could not sent the message to the seller.")
+        }
+
+        resetForm();
+
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: "Awesome!",
+                body: "Your message was sent to the seller."
+            },
+            trigger: {
+                seconds: 1,
+            }
+        })
+    }
+
+    return (
+        <AppForm
+            initialValues={{ message: "" }}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
+        >
+            <AppFormField
+                maxLength={255}
+                multiline
+                name="message"
+                numberOfLines={3}
+                placeholder={"Message..."}
+            />
+            <SubmitButton title="Contact Seller" />
+        </AppForm>
+    )
+};
+
+export default ContactSellerForm;
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,33 +1,48 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import { Image } from 'react-native-expo-image-cache';
 
 import colors from '../config/colors';
 import ListItem from '../components/ListItem';
+import ContactSellerForm from '../components/ContactSellerForm';
+
 
 const ListingDetailsScreen = ({ user, profilepic, listings, route }) => {
     const listing = route.params;
 
+    const handleSubmit = () => console.log('Submitted');
+
     return (
-        <View>
-            <Image
-                style={styles.image}
-                preview={{ uri: listing.images[0].thumbnailUrl }}
-                tint='light'
-                uri={listing.images[0].url}
-            />
-            <View style={styles.itemDetails}>
-                <Text style={styles.title}>{listing.title}</Text>
-                <Text style={styles.subTitle}>${listing.price}</Text>
+        <KeyboardAvoidingView
+            behavior="position"
+            keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+        >
+            <View>
+                <Image
+                    style={styles.image}
+                    preview={{ uri: listing.images[0].thumbnailUrl }}
+                    tint='light'
+                    uri={listing.images[0].url}
+                />
+                <View style={styles.itemDetails}>
+                    <Text style={styles.title}>{listing.title}</Text>
+                    <Text style={styles.subTitle}>${listing.price}</Text>
+                </View>
+                <View style={styles.userContainer}>
+                    <ListItem image={require("../assets/mosh.jpg")} title="Mosh Hamedani" subTitle="5 Listings" />
+                </View>
+                <View style={styles.form}>
+                    <ContactSellerForm listing={listing} />
+                </View>
             </View>
-            <View style={styles.userContainer}>
-                <ListItem image={require("../assets/mosh.jpg")} title="Mosh Hamedani" subTitle="5 Listings" />
-            </View>
-        </View>
+        </ KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
+    form: {
+        paddingHorizontal: 10,
+    },
     image: {
         height: 300,
         width: "100%",
